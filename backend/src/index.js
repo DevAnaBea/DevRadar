48b752236dafe50aab902ff2a3df979a08d6b6ef
Ana Beatriz Thomaz <201908047471@alunos.estacio.br>
2020-02-24 17:18:48 -0300

    funcionando

---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const http = require('http');
+
+const routes = require("./routes");
+const { setupWebSocket } = require("./websocket")
+
+const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
+
+mongoose.connect(
+  "mongodb+srv://bibiabss:omnistack10@cluster0-dunlf.mongodb.net/week10?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
